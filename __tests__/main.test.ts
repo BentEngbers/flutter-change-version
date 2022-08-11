d88969ee23aa19cb9d1669043886c3bae0d4b6f2
Bent Engbers <bentengbers@gmail.com>
2022-08-11 18:43:35 +0200

    initial version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,30 @@
-import {wait} from '../src/wait'
+import {convertToFullPath, parseYamlString, readYamlFile} from '../src/utils'
+import {increaseBuildNumber} from '../src/main'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+function createChildProcess(currentWorkingDirectory: string) {
+  process.env['INPUT_INCREASE-BUILD-NUMBER'] = 'true'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    env: process.env,
+    cwd: currentWorkingDirectory
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  return cp.execFileSync(np, [ip], options).toString()
+}
+test('test runs correctly', () => {
+  const result = createChildProcess('./__tests__/static/')
+  expect(result).toEqual(
+    expect.stringContaining('::set-env name=NEW_VERSION::32.21.43+')
+  )
+})
+test('test runs correctly', () => {
+  expect(() => createChildProcess('fake')).toThrow()
+})
+
+test('e2e typescript test', () => {
+  increaseBuildNumber('./__tests__/static/')
 })
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,41 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {
+  convertToFullPath,
+  fullPathType,
+  parseYamlString,
+  readYamlFile,
+  writeYamlFile
+} from './utils'
+import {Version} from './version'
+
+const baseDir = './'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const shouldIncreaseBuildnumber: boolean =
+      core.getInput('increase-build-number') == 'true'
+    if (!shouldIncreaseBuildnumber) {
+      return
+    }
+    increaseBuildNumber('./')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
+    throw error
   }
+}
+
+export function increaseBuildNumber(path: string) {
+  const fullPath = convertToFullPath(path)
+  const yaml = parseYamlString(readYamlFile(fullPath))
+  const versionString = yaml.get('version')
+  if (typeof versionString !== 'string') {
+    throw Error('Version string nog found')
+  }
+  const version = Version.fromString(versionString)
+  version.buildNumber++
+  core.exportVariable('NEW_VERSION', version.stringify)
+  yaml.set('version', version.stringify)
+  writeYamlFile(fullPath, yaml)
 }
 
 run()
